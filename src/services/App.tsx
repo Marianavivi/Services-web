572a7534ada0b6493fa701d899879a256e3ecc69
Mariana <marianagutierrezvivi@gmail.com>
2024-11-27 17:38:20 +0300

    Update App.tsx

--- src/services/App.tsx
+++ src/services/App.tsx
@@ -1,1 +1,20 @@
 
+import * as React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
