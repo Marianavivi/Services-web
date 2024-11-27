da594aa1d9e5d3ac0dea742f3c33597dbbe90cde
Mariana <marianagutierrezvivi@gmail.com>
2024-11-27 18:18:02 +0300

    Update App.tsx

--- src/services/App.tsx
+++ src/services/App.tsx
@@ -6,6 +6,7 @@ import Products from "./pages/Products";
 import RegisterForm from "./components/auth/RegisterForm";
 import LoginForm from "./components/auth/LoginForm";
 import Cart from "./components/Cart";
+import Checkout from "./components/Checkout"; // Import Checkout component
 
 const App: React.FC = () => {
   return (
@@ -18,6 +19,7 @@ const App: React.FC = () => {
           <Route path="/register" element={<RegisterForm />} />
           <Route path="/login" element={<LoginForm />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} /> {/* Add checkout route */}
         </Routes>
       </div>
     </BrowserRouter>
