fba20b4b900e5ebd2af2cd9a1eea0be27eea3865
Mariana <marianagutierrezvivi@gmail.com>
2024-11-27 17:37:47 +0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,1 +1,13 @@
+import * as React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to My Online Store!</h1>
+      <p>This is the home page.</p>
+    </div>
+  );
+};
+
+export default Home;
 
