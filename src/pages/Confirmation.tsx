6c5bfd14893edcc1e84a485736051c06b21b62dc
Mariana <marianagutierrezvivi@gmail.com>
2024-11-27 18:19:56 +0300

    Create Confirmation.tsx

---
+++ src/pages/Confirmation.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+const Confirmation: React.FC = () => {
+  return (
+    <div>
+      <h2>Order Confirmation</h2>
+      <p>Thank you for your order!</p>
+      <p>You will receive a confirmation email shortly.</p>
+    </div>
+  );
+};
+
+export default Confirmation;
